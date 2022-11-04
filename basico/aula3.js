ad66807dc23aea7ee4bb4c6bc6c56e4d4fe4551c
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-11-04 20:36:25 -0300

    Comando console.log

---
+++ basico/aula3.js
@@ -0,0 +1,10 @@
+// Primeiro comando básico console.log()
+console.log("Olá mundo!");
+
+// Tipos de dados: String (texto)
+console.log("Gabriel 'GXFXL' Dantas");
+console.log(`'Gabriel' "Dantas"`)
+
+// Tipos de dados: Number (Inteiros e decimais)
+console.log(12323);
+console.log(24.45);
