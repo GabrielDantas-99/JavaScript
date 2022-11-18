dfdf92227ded3333a7288658efc6bf3599bd6c69
Gabriel Dantas <gabriel.dantas@quark.tec.br>
2022-11-18 12:43:00 -0300

    asd

---
+++ basico/aula12.js
@@ -0,0 +1,16 @@
+// Objeto Math
+
+let n1 = 9.51251;
+
+let n2 = Math.floor(n1); // Arredonda para baixo
+console.log(n2);
+
+let n3 = Math.ceil(n1); // Arredonda para cima
+console.log(n3);
+
+let n4 = Math.round(n1); // Arredonda para o valor inteiro mais proximo
+console.log(n4);
+
+console.log(Math.max(412, 123, 3213, 5123, -123, -5123, 151, 1, -512));
+
+
