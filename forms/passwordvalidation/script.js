239c2fa0976af27e46c80e0461f927825a737b6a
Gabriel Dantas <gabrieldantaslp@gmail.com>
2022-12-26 17:42:36 -0300

    atualizando

--- forms/passwordvalidation/script.js
+++ forms/passwordvalidation/script.js
@@ -1,65 +1,76 @@
 let is_visible = false;
+let input1 = document.getElementById("j_idt20:senha");
+let input2 = document.getElementById("j_idt20:repetirSenha");
 let oito, maiuscula, minuscula, especial, espacos, iguais = false;
+let botao = document.getElementById("j_idt20:logar");
+botao.setAttribute("disabled", "true")
 
 function ver() {
-  let input = document.getElementById("password");
   let word = document.getElementById("word");
   let ver = document.getElementById("icon");
 
   if (is_visible) {
-    input.type = 'password';
+    input1.type = 'password';
+    input2.type = 'password';
     is_visible = false;
     word.textContent = "Ver Senha";
-    ver.innerHTML = "visibility"
-  }
-  else {
-    input.type = "text";
+    ver.classList.replace("fa-eye-slash", "fa-eye");
+  } else {
+    input1.type = "text";
+    input2.type = "text";
     is_visible = true;
     word.textContent = "Ocultar Senha";
-    ver.innerHTML = "visibility_off"
+    ver.classList.replace("fa-eye", "fa-eye-slash");
   }
 }
 
 function check() {
   /*Validações para aceitar nova senha*/
-  let input = document.getElementById("password").value;
-  let input2 = document.getElementById("r_password").value;
-
-  validacao("check0", input.length >= 8) ? oito = true : oito = false;
-  validacao("check1", /[A-Z]/.test(input)) ? maiuscula  = true : maiuscula  = false;
-  validacao("check2", /[a-z]/.test(input)) ? minuscula  = true : minuscula  = false;
-  validacao("check3", /\W|_/.test(input)) ? especial  = true : especial  = false;
-  validacao("check4", !/\s/g.test(input) && (input.length > 0)) ? espacos  = true : espacos  = false;
-  validacao("check5", (input == input2) && (input.length > 0)) ? iguais  = true : iguais  = false;
-  console.log("----------")
-  console.log(oito);
-  console.log(maiuscula);
-  console.log(minuscula);
-  console.log(especial);
-  console.log(espacos);
-  console.log(iguais);
-  finalizar();
+  let valor1 = input1.value;
+  let valor2 = input2.value;
+  validacao("check0", valor1.length >= 8) ? oito = true : oito = false;
+  validacao("check1", /[A-Z]/.test(valor1)) ? maiuscula = true : maiuscula = false;
+  validacao("check2", /[a-z]/.test(valor1)) ? minuscula = true : minuscula = false;
+  validacao("check3", /\W|_/.test(valor1)) ? especial = true : especial = false;
+  validacao("check4", !/\s/g.test(valor1) && (valor1.length > 0)) ? espacos = true : espacos = false;
+  validacao("check5", (valor1 == valor2) && (valor1.length > 0)) ? iguais = true : iguais = false;
+  checkFinalizar();
+  // console.log(oito);
+  // console.log(maiuscula);
+  // console.log(minuscula);
+  // console.log(especial);
+  // console.log(espacos);
+  // console.log(iguais);
 }
 
-function validacao (elemento, validacao) {
+function validacao(elemento, validacao) {
   elemento = document.getElementById(elemento);
   let finalizar = false;
   validacao ? finalizar = passou() : finalizar = naoPassou();
+
   function passou() {
-    elemento.style.color="#41d442";
+    elemento.style.color = "#41d442";
     return true;
   }
   function naoPassou() {
-    elemento.style.color="#ca0606";
+    elemento.style.color = "#ca0606";
     return false;
   }
   return finalizar;
 }
 
-function finalizar() {
-  let botao = document.getElementById("j_idt20:logar");
-  
+function checkFinalizar() {
+  console.log(botao);
   let permissoes = [oito, maiuscula, minuscula, especial, espacos, iguais];
-  console.log("---" + permissoes.includes(false));
-  !permissoes.includes(false) ? botao.classList.replace("disabled", "enabled") : botao.classList.replace("enabled", "disabled");
+  if (!permissoes.includes(false)) {
+    botao.removeAttribute("disabled");
+    botao.classList.replace("disabled", "enabled");
+  } else {
+    botao.setAttribute("disabled", "true");
+    botao.classList.replace("enabled", "disabled");
+  }
+}
+
+function salvar() {
+  window.location.href = "sucesso.html";
 }
